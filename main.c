19718e42f1a0c0543ce5ddbd8fffdbc8896c87cc
Kartik Pingale <kartikpgr8@gmail.com>
2017-09-08 22:02:42 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,4 +2,10 @@
 
 int main()
 {
+  printf("Select the operation\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Transpose\n5. Determinant\n6. Cofactor\n7. Rank\n8. Trace");
+  int m, n;
+  printf("Enter the number of rows and columns: ");
+  scanf("%d %d", &m, &n);
+  int a[m][n];
+  return 0;
 }
