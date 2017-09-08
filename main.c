e8bac1f9714b775b82d86a9ff48564ad0fd26b31
Kartik Pingale <kartikpgr8@gmail.com>
2017-09-08 22:14:59 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,7 +2,14 @@
 
 int main()
 {
-  printf("Select the operation\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Transpose\n5. Determinant\n6. Cofactor\n7. Rank\n8. Trace");
+  printf("Select the operation\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Transpose\n");
+  printf("5. Determinant\n6. Cofactor\n7. Inverse\n8. Rank\n");
+  int choice;
+  scanf("%d", &choice);
+  switch(choice)
+  {
+    case 1:
+  }
   int m, n;
   printf("Enter the number of rows and columns: ");
   scanf("%d %d", &m, &n);
